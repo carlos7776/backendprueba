806b9dac6b6c6037846db954cf42be1e9f4a8d74
SISTEMAS 3 <you@example.com>
2022-10-04 17:56:55 -0500

    ensayobk

---
+++ BackendDelivery/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+const passport = require('passport') 
+ 
+/*
+* RUTAS
+*/
+const users = require('./routes/usersRoutes');
+ 
+const port = process.env.PORT || 3001;
+ 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+ 
+app.use(cors());
+app.use(passport.initialize()); 
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+
+
+app.disable('x-powered-by');
+ 
+app.set('port', port);
+ 
+/*
+* LLAMANDO A LAS RUTAS
+*/
+users(app);
+ 
+server.listen(3001, '10.183.144.128' || 'localhost', function() {
+    console.log('Aplicacion de NodeJS ' + port + ' Iniciada...')
+});
+
+app.get('/',(req, res) => {
+    res.send('Ruta raiz')
+});
+app.get('/test',(req, res) => {
+    res.send('esta es una ruta de testeo')
+});
+ 
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+ 
+module.exports = {
+    app: app,
+    server: server
+}
+ 
+// 200 - ES UN RESPUESTA EXITOSA
+// 404 - SIGNIFICA QUE LA URL NO EXISTE
+// 500 - ERROR INTERNO DEL SERVIDOR
